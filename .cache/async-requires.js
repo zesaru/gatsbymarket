90f9a1e2b7b2205cb84d1117d2deeea83b1bfec7
Cesar Murillo <namiayumis@gmail.com>
2019-08-01 22:41:21 +0900

    first component

---
+++ .cache/async-requires.js
@@ -0,0 +1,8 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/cesarmurillo/code/platzi/primersitiogatsby/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-index-js": () => import("/Users/cesarmurillo/code/platzi/primersitiogatsby/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
